1c8298503a8d5bd9a0725facd38a2d2179f0d159
Danah Dykstra <70775448+dcdykstra@users.noreply.github.com>
2022-12-30 14:21:56 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from loguru import logger as log
-from api.scrape import VLR_Scraper
-from schema import Segments_Player_Stats, Segments_Round_Stats, Segments_Event_URLS, Segments_Match_URLS
+from api.match_statistics import Match_Stats_Scraper
+from api.match_economy import Match_Economy_Scraper
+from api.url import URL_Scraper
+from schema import *
 
 app = FastAPI(
     title="VLR.gg API",
@@ -10,30 +11,15 @@ app = FastAPI(
     docs_url="/",
     redoc_url=None,
 )
-vlr = VLR_Scraper()
+
+ms_s = Match_Stats_Scraper()
+me_s = Match_Economy_Scraper()
+url_s = URL_Scraper()
 
 
-@app.get("/url/event/{region}/{event_type}", response_model=Segments_Event_URLS)
-async def event_url(region: str, event_type: str):
-    """
-    region:\n
-        all\n
-        north-america\n
-        europe\n
-        brazil\n
-        asia-pacific\n
-        korea\n
-        japan\n
-        latin-america\n
-        oceania\n
-        mena\n
-        game-changers\n
-        collegiate
-    event_type:\n
-        completed\n
-        upcoming
-    """
-    return vlr.get_event_urls(region, event_type)
+@app.get("/url/event/{region}/{event_type}/{page}", response_model=Segments_Event_URLS)
+async def event_url(region: Region, event_type: Event_Type, page: int):
+    return url_s.get_event_urls(region, event_type, page)
 
 
 @app.get("/url/match/{event_code}", response_model=Segments_Match_URLS)
@@ -44,7 +30,7 @@ async def match_url(event_code: str):
         https://www.vlr.gg/event/1015/valorant-champions-2022/playoffs\n
         The event code for this page is: 1015
     """
-    return vlr.get_match_urls(event_code)
+    return url_s.get_match_urls(event_code)
 
 
 @app.get("/stats/{match_code}/rounds", response_model=Segments_Round_Stats)
@@ -55,19 +41,26 @@ async def match_stats_rounds(match_code: str):
         https://www.vlr.gg/130685/loud-vs-optic-gaming-valorant-champions-2022-gf\n
         The match code for this page is: 130685
     """
-    return vlr.get_match_stats_round_order(match_code)
+    return ms_s.get_match_stats_round_order(match_code)
 
 
-@app.get("/stats/{match_code}/{side}", response_model=Segments_Player_Stats)
-async def match_stats_players(match_code: str, side: str):
+@app.get("/stats/{match_code}/players/{side}", response_model=Segments_Player_Stats)
+async def match_stats_players(match_code: str, side: Side):
     """
     match_code:\n
         Match pages look like:\n
         https://www.vlr.gg/130685/loud-vs-optic-gaming-valorant-champions-2022-gf\n
         The match code for this page is: 130685\n
-    side:\n
-        both\n
-        t\n
-        ct\n
     """
-    return vlr.get_match_stats_players(match_code, side)
+    return ms_s.get_match_stats_players(match_code, side)
+
+
+@app.get("/stats/{match_code}/economy", response_model=Segments_Economy_Stats)
+async def match_stats_economy(match_code: str):
+    """
+    match_code:\n
+        Match pages look like:\n
+        https://www.vlr.gg/130685/loud-vs-optic-gaming-valorant-champions-2022-gf\n
+        The match code for this page is: 130685
+    """
+    return me_s.get_economy_data(match_code)
--- schema.py
+++ schema.py
@@ -1,11 +1,38 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional
+from enum import Enum
+
+
+class Event_Type(str, Enum):
+    COMPLETED = "completed"
+    UPCOMING = "upcoming"
+
+
+class Region(str, Enum):
+    ALL = "all"
+    NA = "north-america"
+    EU = "europe"
+    BR = "brazil"
+    APAC = "asia-pacific"
+    KR = "korea"
+    JP = "japan"
+    LATAM = "latin-america"
+    OCE = "oceania"
+    MENA = "mena"
+    GC = "game-changers"
+    COLL = "collegiate"
+
+
+class Side(str, Enum):
+    BOTH = "both"
+    CT = "ct"
+    T = "t"
 
 
 class Player_Statistics(BaseModel):
     Player: str
     Agent: str
-    Rating: int = Field(description="VLR Rating")
+    Rating: Optional[int] = Field(description="VLR Rating")
     ACS: int = Field(alias="Average Combat Score")
     Kills: int
     Deaths: int
@@ -40,38 +67,38 @@ class Match_Statistics(BaseModel):
 class Round_Statistics(BaseModel):
     url: str = Field(description="URL for the match")
     map: str = Field(description="Map for the respective match")
-    r1: str = Field(description="Winning Side", alias="Round 1")
-    r2: str = Field(description="Winning Side", alias="Round 2")
-    r3: str = Field(description="Winning Side", alias="Round 3")
-    r4: str = Field(description="Winning Side", alias="Round 4")
-    r5: str = Field(description="Winning Side", alias="Round 5")
-    r6: str = Field(description="Winning Side", alias="Round 6")
-    r7: str = Field(description="Winning Side", alias="Round 7")
-    r8: str = Field(description="Winning Side", alias="Round 8")
-    r9: str = Field(description="Winning Side", alias="Round 9")
-    r10: str = Field(description="Winning Side", alias="Round 10")
-    r11: str = Field(description="Winning Side", alias="Round 11")
-    r12: str = Field(description="Winning Side", alias="Round 12")
-    r13: str = Field(description="Winning Side", alias="Round 13")
-    r14: Optional[str] = Field(description="Winning Side", alias="Round 14")
-    r15: Optional[str] = Field(description="Winning Side", alias="Round 15")
-    r16: Optional[str] = Field(description="Winning Side", alias="Round 16")
-    r17: Optional[str] = Field(description="Winning Side", alias="Round 17")
-    r18: Optional[str] = Field(description="Winning Side", alias="Round 18")
-    r19: Optional[str] = Field(description="Winning Side", alias="Round 19")
-    r20: Optional[str] = Field(description="Winning Side", alias="Round 20")
-    r21: Optional[str] = Field(description="Winning Side", alias="Round 21")
-    r22: Optional[str] = Field(description="Winning Side", alias="Round 22")
-    r23: Optional[str] = Field(description="Winning Side", alias="Round 23")
-    r24: Optional[str] = Field(description="Winning Side", alias="Round 24")
+    winner: str = Field(description="Winning team")
+    team1: str = Field(description="Team 1 Full Name")
+    team2: str = Field(description="Team 2 Full Name")
+    team1_ct_score: int = Field(
+        description="Total rounds team 1 won on ct side")
+    team2_ct_score: int = Field(
+        description="Total rounds team 2 won on ct side")
+    team1_t_score: int = Field(description="Total rounds team 1 won on t side")
+    team2_t_score: int = Field(description="Total rounds team 2 won on t side")
+    team1_starting_side: str = Field(description="Side team 1 started on")
+    team2_starting_side: str = Field(description="Side team 2 started on")
+    round_order_data: dict
+
+
+class Economy_Statistics(BaseModel):
+    url: str = Field(description="URL for the match")
+    map: str = Field(description="Map for the respective match")
+    team1: str = Field(description="Team 1 Short Name")
+    team2: str = Field(description="Team 2 Short Name")
+    team1_spending: dict
+    team2_spending: dict
+
 
 class Segments_Match_URLS(BaseModel):
     status: int
     urls: list = Field(description="List of Valorant match URLS")
 
+
 class Segments_Event_URLS(BaseModel):
     status: int
     urls: list = Field(description="List of Valorant event URLS")
+
 
 class Segments_Player_Stats(BaseModel):
     status: int
@@ -81,3 +108,8 @@ class Segments_Player_Stats(BaseModel):
 class Segments_Round_Stats(BaseModel):
     status: int
     segments: List[Round_Statistics]
+
+
+class Segments_Economy_Stats(BaseModel):
+    status: int
+    segments: List[Economy_Statistics]
